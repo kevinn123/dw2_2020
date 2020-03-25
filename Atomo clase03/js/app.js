1b356d11f5faf3d609c2c863a2068433c9265054
kevinn123 <61994222+kevinn123@users.noreply.github.com>
2020-03-26 00:31:04 +0100

    Add files via upload

---
+++ Atomo clase03/js/app.js
@@ -0,0 +1,27 @@
+var numero=5;
+console.log("Imprimiendo... tabla del 5");
+for( var k = 0 ; k < 11 ;k++)
+{
+  resultado=numero*k;
+  console.log(resultado);
+}
+
+var numero_2=5;
+var numero_3=6;
+if (numero_2<numero_3)
+{
+  console.log("ESTA ES LA TABLA DEL 5");
+  for(var k = 0 ; k < 11 ; k++){
+    resultado_2=numero_2*k;
+    console.log(resultado_2);
+}
+  console.log("ESTA ES LA TABLA DEL 6");
+    for(var k = 0 ; k < 11 ; k++)
+{
+      resultado_3=numero_3*k;
+      console.log(resultado_3);
+}
+} else
+{
+      console.log("System Error");
+}
